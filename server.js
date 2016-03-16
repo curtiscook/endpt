a5915dcecf2037e0b317d8156f87bf9954801581
Curtis Cook <curtis@blendlabsinc.com>
2016-03-15 22:10:13 -0400

    coment out sendgrid

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var sendgrid = require("sendgrid")(process.env.SEND_GRID);
-var email = new sendgrid.Email();
+//var sendgrid = require("sendgrid")(process.env.SEND_GRID);
+//var email = new sendgrid.Email();
 var Slack = require('slack-node');
 
 var apiToken = process.env.API_TOKEN;
 var slack = new Slack(apiToken);
 
-email.addTo("curtis@blendlabs.com");
-email.setFrom("noreply@blendlabs.com");
-email.setSubject("Webhooks Test");
+//email.addTo("curtis@blendlabs.com");
+//email.setFrom("noreply@blendlabs.com");
+//email.setSubject("Webhooks Test");
 
 app.use(express.bodyParser());
 
@@ -29,7 +29,7 @@ app.post('/', function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     var reqText = JSON.stringify(req.body, null, '\t');
     console.log(reqText);
-    email.setText(reqText);
+ //   email.setText(reqText);
 
     slack.api('chat.postMessage', {
       username: '@bailey',
@@ -40,10 +40,10 @@ app.post('/', function(req, res){
     });
 
 
-    sendgrid.send(email, function(err, json) {
-        if (err) { return console.error(err); }
-          console.log(json);
-    });
+//    sendgrid.send(email, function(err, json) {
+//        if (err) { return console.error(err); }
+//          console.log(json);
+//    });
     res.end('thanks');
 });
 
